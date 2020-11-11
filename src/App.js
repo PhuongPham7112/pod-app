60cf84fc30bc283669ab27d130711bbba821affd
Nia Ignacio <niaignacio@gmail.com>
2020-11-11 00:59:21 -0800

    Practice Update
    testing out how to make changes using git

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          This is my testing text!
         </p>
         <a
           className="App-link"
