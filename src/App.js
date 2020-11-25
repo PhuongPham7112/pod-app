3b6cbab01c45a5f575d916fa1af3b12b890138af
Your Name <alassycottery@gmail.com>
2020-11-25 10:06:12 +0700

    "save"

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Button } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import AppImages from './AppImages.js';
+
+const Title = (props) => {
+  return (
+    <div style={{backgroundColor: "#c9aae8", color: "black", margin: "5vh", width: "100%", fontSize: "2rem"}}> 
+      <h1> {props.title} </h1>
+    </div>
+  )
+}
+
+const Links = () => {
+  return (
+    <ul className="link-list">
+      <li className="link"><a href="https://www.youtube.com/watch?v=olRi5-AOn1A"> Features </a></li>
+      <li className="link"><a href="https://www.youtube.com/watch?v=g2mLOVHZ2u4"> Subscribe </a></li>
+      <li className="link"><a href="https://www.youtube.com/watch?v=xuCn8ux2gbs"> Contacts </a></li>
+      <li className="link" style={{color: "black"}}>© 2020 Pod Co. All rights reserved</li>
+    </ul>
+  )
+}
+
+const useStyles = makeStyles({
+  root: {
+    background: '#c9aae8',
+    border: 0,
+    color: 'black',
+    height: 48,
+    padding: '0 30px',
+  },
+  label: {
+    textTransform: 'capitalize',
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Title title="How Does Pod Differ From Other Platforms? "/>
+        <AppImages/>
+        <Button className="button" classes={{
+        root: classes.root, 
+        label: classes.label,}}> 
+          Subscribe to our mail list! 
+        </Button>
+        <Title title="Meet the Team"/>
+        <img src="/images/team.png" className="team-image" alt=""/>
+        <Box marginTop={5} marginBottom={5}>
+          <Button className="button" classes={{
+          root: classes.root, 
+          label: classes.label,}}> 
+            Request pitch deck from us 
+          </Button>
+        </Box>
+        <Links/>
       </header>
     </div>
   );
--- src/AppImages.js
+++ src/AppImages.js
@@ -5,9 +5,9 @@ import Grid from '@material-ui/core/Grid';
 const AppImages = () => {
     return (
       <div className="container">
-        <Grid container spacing={4} direction="row" justify="center" alignItems="center"> 
+        <Grid container direction="row" justify="center" alignItems="center"> 
           <Grid item lg={4}>
-            <img src="/images/reddit.webp" className="logo" alt="Reddit"/>
+            <img src="/images/Reddit.png" className="logo" alt="Reddit"/>
             <h2> Reddit </h2>
             <p className="description"> Pros: Communities centered around certain topics </p>
             <p className="description"> Cons: Text based -- barrier to authentic conversations </p>
@@ -26,16 +26,22 @@ const AppImages = () => {
             <p className="description"> Pros: Live-streaming content </p>
             <p className="description"> Cons: Few topics outside of gaming </p>
           </Grid>
+
+          <hr style={{color: "#d3d3d3", width: "90%", marginTop: "5vh"}}></hr>
   
-          <Grid container spacing={10} direction="row" justify="center" alignItems="center"> 
-            <Grid item lg={3}>
+          <Grid container direction="row" justify="center" alignItems="center" style={{width: "100%"}}> 
+            <Grid item lg={4}>
               <img  src="/images/Pod-logo.png" className="pod-logo" alt="Pod"/>
             </Grid>
             <Grid item lg={3}>
               <h2> Pod </h2>
-              <p className="description"> 
-              Community centered around certain topics. Community-based podcasts. Audio-based -- more authentic conversations. Wide-variety of topics. Podcasts organized by topic
-              </p>
+              <ul className="description" style={{textAlign: "left"}}>
+                <li>Community centered around certain topics</li>
+                <li>Community-based podcasts</li>
+                <li>Audio-based -- more authentic conversations</li>
+                <li>Wide-variety of topics</li>
+                <li>Podcasts organized by topic</li>
+              </ul>
             </Grid>
           </Grid>
         </Grid>
